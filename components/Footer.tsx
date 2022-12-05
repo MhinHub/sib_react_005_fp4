0e9ba248d39e48e4e0bd30097426da4daed11b9b
MinlabHQ <iskandarm20d@student.unhas.ac.id>
2022-12-05 20:00:09 +0800

    change display footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
 
     return (
         <footer className='bottom-0'>
-            <div className="flex justify-center items-center py-4">
+            <div className="flex-1 justify-center items-center py-4">
                 <p className="text-sm text-gray-400">© {year} Mouvee - All rights reserved</p>
             </div>
         </footer>
