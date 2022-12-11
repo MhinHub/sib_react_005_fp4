ac4440b53a623b17d319c739d9441aed157c036b
MinlabHQ <iskandarm20d@student.unhas.ac.id>
2022-12-11 19:27:19 +0800

    add icon on card thumbnail

--- components/molecules/Thumbnail.tsx
+++ components/molecules/Thumbnail.tsx
@@ -7,6 +7,8 @@ import { Movie } from '@typings'
 import { DocumentData } from 'firebase/firestore'
 import Link from 'next/link'
 import { memo } from 'react'
+import { PlayCircle, ExportSquare } from 'iconsax-react'
+import { Tooltip } from 'flowbite-react'
 
 interface Props {
   // When using firebase
@@ -30,22 +32,52 @@ function Thumbnail({ movie, children, styleImg }: Props) {
 
   return (
     <div
-      className={`relative h-28 min-w-[280px] cursor-pointer transition duration-200 ease-out md:h-48 md:w-full md:hover:scale-105`}
-      onClick={() => {
-        setCurrentMovie(movie)
-        setShowModal(true)
-      }}
+      className={`relative group/thumb h-28 min-w-[280px] transition duration-200 ease-out md:h-48 md:w-full md:hover:scale-105`}
     >
-      <Link href={`/detail/${movie.id}`}>
-        <Image
-          src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path || movie.poster_path
-            }`}
-          className={`${styleImg} rounded-sm object-cover`}
-          fill
-          loading='lazy'
-          sizes='100%'
-          alt="Mouvee thumbnail"
-        />
+      <Image
+        src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path || movie.poster_path
+          }`}
+        className={`${styleImg} rounded-sm object-cover`}
+        fill
+        loading='lazy'
+        sizes='100%'
+        alt="Mouvee thumbnail"
+      />
+      <div className='invisible group-hover/thumb:visible absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
+        <Tooltip
+          content="Play Trailer"
+          style="dark"
+          placement="top"
+          className='w-fit text-center text-sm'
+        >
+          <PlayCircle
+            className='text-white cursor-pointer'
+            size={40}
+            variant='Bulk'
+            onClick={() => {
+              setCurrentMovie(movie)
+              setShowModal(true)
+            }}
+          />
+        </Tooltip>
+      </div>
+
+      <Link
+        href={`/detail/${movie.id}`}
+        className='invisible group-hover/thumb:visible absolute top-1/2 right-2 transform -translate-y-1/2'
+      >
+        <Tooltip
+          content="Detail Movie"
+          style="dark"
+          placement="top"
+          className='w-fit text-center text-sm'
+        >
+          <ExportSquare
+            className='text-white cursor-pointer'
+            size={20}
+            variant='TwoTone'
+          />
+        </Tooltip>
       </Link>
       {children}
     </div>
