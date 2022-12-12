569efa2d27b1ce4549033b6f171a0f1a75551390
MinlabHQ <iskandarm20d@student.unhas.ac.id>
2022-12-12 14:17:32 +0800

    update navbar

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,10 @@
-import { BellIcon, SearchIcon } from '@heroicons/react/solid'
 import { SearchNormal1 } from 'iconsax-react'
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
 import useAuth from '../hooks/useAuth'
-import BasicMenu from './molecules/BasicMenu'
 import { memo } from 'react'
 import { useRouter } from 'next/navigation'
+import Image from 'next/image'
 
 function Header() {
   const [isScrolled, setIsScrolled] = useState(false)
@@ -39,36 +38,28 @@ function Header() {
 
   return (
     <header className={`${isScrolled ? 'bg-glass-gray h-[10vh] w-full rounded-md' : 'hidden transition'} transition`}>
-      <div className="flex items-center space-x-2 md:space-x-10">
-        <img
-          src="https://rb.gy/ulxxee"
-          width={100}
-          height={100}
+      <div className="flex items-center">
+        <Image
+          src="/static/logo-mouvee.png"
+          width={80}
+          height={80}
           className="cursor-pointer object-contain"
+          alt='logo-mouvee'
         />
-        <BasicMenu />
-        <ul className="hidden space-x-4 md:flex">
-          <li className="headerLink">Home</li>
-          <li className="headerLink">TV Shows</li>
-          <li className="headerLink">Movies</li>
-          <li className="headerLink">New & Popular</li>
-          <li className="headerLink">My List</li>
-        </ul>
+        <p className="text-2xl font-bold text-gray-100 invisible md:visible">Mouvee</p>
       </div>
 
       <div className="flex items-center space-x-4 text-sm font-light">
-        <form onSubmit={handleSearch} className="flex group items-center justify-end">
+        <form onSubmit={handleSearch} className="flex group/nav items-center justify-end">
           <input
             type="text"
             placeholder="Search movie..."
-            className="bg-glass-gray w-96 h-10 rounded-full px-5"
+            className="bg-glass-gray w-48 h-10 md:w-96 rounded-full px-5"
             value={keyword}
             onChange={(e) => setKeyword(e.target.value)}
           />
-          <SearchNormal1 className='text-gray-100 absolute mr-5 group-active:invisible' variant="TwoTone" />
+          <SearchNormal1 className='text-gray-100 absolute mr-5 group-active/nav:invisible' variant="TwoTone" />
         </form>
-        <p className="hidden lg:inline">Kids</p>
-        <BellIcon className="h-6 w-6" />
         <img
           onClick={logout}
           src="https://rb.gy/g1pwyx"
