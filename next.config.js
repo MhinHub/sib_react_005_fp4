560e18fd531bb75f1e5d847ddb8dc437e94dd1ff
MinlabHQ <iskandarm20d@student.unhas.ac.id>
2022-12-13 20:49:55 +0800

    make this app as PWA

--- next.config.js
+++ next.config.js
@@ -1,7 +1,30 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   images: {
     domains: ['image.tmdb.org', 'rb.gy', 'www.gravatar.com'],
   },
+  webpack: (config) => {
+    config.infrastructureLogging = {
+      debug: /webpack\.config\.js/,
+    };
+    return config;
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
+
+const prod = process.env.NODE_ENV === 'production'
+
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: prod ? false : true,
+})
+
+module.exports = withPWA(nextConfig);
---
+++ pages/_document.tsx
@@ -0,0 +1,39 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import { DocumentContext } from "next/document";
+
+export default class MyDocument extends Document {
+    static async getInitialProps(ctx: DocumentContext) {
+        const originalRenderPage = ctx.renderPage;
+
+        // Run the React rendering logic synchronously
+        ctx.renderPage = () =>
+            originalRenderPage({
+                // Useful for wrapping the whole react tree
+                enhanceApp: (App) => App,
+                // Useful for wrapping in a per-page basis
+                enhanceComponent: (Component) => Component,
+            });
+
+        // Run the parent `getInitialProps`, it now includes the custom `renderPage`
+        const initialProps = await Document.getInitialProps(ctx);
+
+        return initialProps;
+    }
+    render() {
+        return (
+            <Html lang="en" className="scroll-smooth">
+                <Head>
+                    <meta charSet="UTF-8" />
+                    <link rel="manifest" href="/manifest.json" />
+                    <link rel="apple-touch-icon" href="/icon-192x192.png" />
+                    <meta name="theme-color" content="#9013fe" />
+                    <meta name="msapplication-navbutton-color" content="#9013fe" />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
