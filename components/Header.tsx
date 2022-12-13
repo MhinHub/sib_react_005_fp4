02c05a71ffff7ab37e56ded48415b9a42b604fad
MinlabHQ <iskandarm20d@student.unhas.ac.id>
2022-12-13 21:20:32 +0800

    make this app as PWA -fix

--- components/Header.tsx
+++ components/Header.tsx
@@ -42,63 +42,71 @@ function Header() {
 
 
   return (
-    <header className={`${isScrolled ? 'bg-glass-gray h-[10vh] w-full rounded-md' : 'hidden transition'} transition ease-in-out duration-75`}>
-      <Link href="/" className="flex items-center">
-        <Image
-          src="/static/logo-mouvee.png"
-          width={80}
-          height={80}
-          className="cursor-pointer object-contain"
-          alt='logo-mouvee'
-        />
-        <p className="text-2xl font-bold text-gray-100 invisible md:visible">Mouvee</p>
-      </Link>
-
-      <div className="flex items-center space-x-4 text-sm font-light">
-        <form onSubmit={handleSearch} className="flex group/nav items-center justify-end">
-          <input
-            type="text"
-            placeholder="Search movie..."
-            className="bg-glass-gray w-60 h-10 md:w-96 rounded-full px-5"
-            value={keyword}
-            onChange={(e) => setKeyword(e.target.value)}
+    <>
+      <header className={`${isScrolled ? 'bg-glass-gray h-[10vh] w-full rounded-md' : 'hidden transition'} transition ease-in-out duration-75`}>
+        <Link href="/" className="flex items-center">
+          <Image
+            src="/static/logo-mouvee.png"
+            width={80}
+            height={80}
+            className="cursor-pointer object-contain"
+            alt='logo-mouvee'
           />
-          <SearchNormal1 className='text-gray-100 absolute mr-5 group-active/nav:invisible' variant="TwoTone" />
-        </form>
-
-        <Link href="/watchlist" className='hidden md:block'>
-          <button className='flex justify-end'>
-            <Archive className='bg-glass-gray p-2 rounded-full cursor-pointer hover:bg-white/10' size={40} variant="Bold" />
-            <span className="flex absolute justify-center items-center w-4 h-4 text-xs font-medium p-2 text-white bg-red-500 rounded-full">{list.length}</span>
-          </button>
+          <p className="text-2xl font-bold text-gray-100 invisible md:visible">Mouvee</p>
         </Link>
 
-        <div className='hidden md:block'>
-        <Dropdown
-          label={<Profile variant='Bulk' size={40} className='bg-glass-gray hover:bg-white/10 p-2 rounded-full' />}
-          inline={true}
-          arrowIcon={false}
-          color="dark"
-        >
-          <Dropdown.Header>
-            <span className="block text-sm">
-              Signed in as
-            </span>
-            <span className="block text-sm font-medium truncate">
-              {user?.email}
-            </span>
-          </Dropdown.Header>
-          <Dropdown.Divider />
-          <Dropdown.Item
-            onClick={logout}
-          >
-            Sign out
-          </Dropdown.Item>
-          </Dropdown>
-        </div>
+        <div className="flex items-center space-x-4 text-sm font-light">
+          <form onSubmit={handleSearch} className="flex group/nav items-center justify-end">
+            <input
+              type="text"
+              placeholder="Search movie..."
+              className="bg-glass-gray w-60 h-10 md:w-96 rounded-full px-5"
+              value={keyword}
+              onChange={(e) => setKeyword(e.target.value)}
+            />
+            <SearchNormal1 className='text-gray-100 absolute mr-5 group-active/nav:invisible' variant="TwoTone" />
+          </form>
 
+          <Link href="/watchlist" className='hidden md:block'>
+            <button className='flex justify-end'>
+              <Archive className='bg-glass-gray p-2 rounded-full cursor-pointer hover:bg-white/10' size={40} variant="Bold" />
+              <span className="flex absolute justify-center items-center w-4 h-4 text-xs font-medium p-2 text-white bg-red-500 rounded-full">{list.length}</span>
+            </button>
+          </Link>
+
+          <div className='hidden md:block'>
+            <Dropdown
+              label={<Profile variant='Bulk' size={40} className='bg-glass-gray hover:bg-white/10 p-2 rounded-full' />}
+              inline={true}
+              arrowIcon={false}
+              color="dark"
+            >
+              <Dropdown.Header>
+                <span className="block text-sm">
+                  Signed in as
+                </span>
+                <span className="block text-sm font-medium truncate">
+                  {user?.email}
+                </span>
+              </Dropdown.Header>
+              <Dropdown.Divider />
+              <Dropdown.Item
+                onClick={logout}
+              >
+                Sign out
+              </Dropdown.Item>
+            </Dropdown>
+          </div>
+
+        </div>
+      </header>
+
+      {/* Bottom Navigation for Mobile */}
+      <div className="nav-bottom">
+        <div className="flex items-center justify-between px-4">
+        </div>
       </div>
-    </header>
+    </>
   )
 }
 
