a7f9666b1a30788c3b789ef8696e8fb5cbdcc953
MhinHub <iskandarm20d@student.unhas.ac.id>
2024-01-18 12:03:39 +0800

    update: link to detail movie

--- components/molecules/Thumbnail.tsx
+++ components/molecules/Thumbnail.tsx
@@ -22,38 +22,42 @@ function Thumbnail({ movie, children, styleImg }: Props) {
   // const [showModal, setShowModal] = useRecoilState(modalState)
   // const [currentMovie, setCurrentMovie] = useRecoilState(movieState)
 
-  const { setIsModalState: setShowModal, setMovieState: setCurrentMovie } = useStore(
-    (state: any) => ({
-      setIsModalState: state.setIsModalState,
-      setMovieState: state.setMovieState
-    }),
-    shallow
-  )
+  const { setIsModalState: setShowModal, setMovieState: setCurrentMovie } =
+    useStore(
+      (state: any) => ({
+        setIsModalState: state.setIsModalState,
+        setMovieState: state.setMovieState,
+      }),
+      shallow
+    )
 
   return (
     <div
-      className={`relative group/thumb h-40 min-w-[280px] transition duration-200 ease-out md:h-50 md:w-full md:hover:scale-105`}
+      className={`group/thumb md:h-50 relative h-40 min-w-[280px] transition duration-200 ease-out md:w-full md:hover:scale-105`}
     >
-      <Image
-        src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path || movie.poster_path
+      <Link href={`/detail/${movie.id}`}>
+        <Image
+          src={`https://image.tmdb.org/t/p/w500${
+            movie.backdrop_path || movie.poster_path
           }`}
-        className={`${styleImg} rounded-t-xl object-cover`}
-        fill
-        loading='lazy'
-        sizes='100%'
-        alt="Mouvee thumbnail"
-      />
-      <div className='md:invisible group-hover/thumb:visible absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
+          className={`${styleImg} rounded-t-xl object-cover`}
+          fill
+          loading="lazy"
+          sizes="100%"
+          alt="Mouvee thumbnail"
+        />
+      </Link>
+      <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform md:invisible group-hover/thumb:visible">
         <Tooltip
           content="Play Trailer"
           style="dark"
           placement="top"
-          className='w-fit z-50 text-center text-sm'
+          className="z-50 w-fit text-center text-sm"
         >
           <PlayCircle
-            className='text-white cursor-pointer'
+            className="cursor-pointer text-purple-300"
             size={40}
-            variant='Bulk'
+            variant="Bulk"
             onClick={() => {
               setCurrentMovie(movie)
               setShowModal(true)
@@ -64,18 +68,18 @@ function Thumbnail({ movie, children, styleImg }: Props) {
 
       <Link
         href={`/detail/${movie.id}`}
-        className='md:invisible group-hover/thumb:visible absolute top-1/2 right-2 transform -translate-y-1/2'
+        className="absolute right-2 top-1/2 -translate-y-1/2 transform md:invisible group-hover/thumb:visible"
       >
         <Tooltip
           content="Detail Movie"
           style="dark"
           placement="top"
-          className='w-fit z-50 text-center text-sm'
+          className="z-50 w-fit text-center text-sm"
         >
           <ExportSquare
-            className='text-white cursor-pointer'
+            className="cursor-pointer text-white"
             size={20}
-            variant='TwoTone'
+            variant="TwoTone"
           />
         </Tooltip>
       </Link>
